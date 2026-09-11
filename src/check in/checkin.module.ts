8da5b0523558a3e6e4a737a43640a319a39b19be
imal33k <ibrahimuye310@gmail.com>
2026-09-11 10:13:27 +0100

    alot

--- src/check in/checkin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CheckInController } from './checkin.controller';
-import { CheckInService } from './checkin.service';
-
-@Module({
-  controllers: [CheckInController],
-  providers: [CheckInService],
-})
-export class CheckInModule {}
--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap(): Promise<void> {
     .filter(Boolean);
 
   app.enableCors({
-    origin: ['https://eventfrontend.pages.dev', 'http://localhost:3000'],
+    origin: ['https://eventfront.pages.dev', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
